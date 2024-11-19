7c765fbb9fbef15d9329f6e14b4bd89dd70da87a
Yamadashoei <k023g0053@g.neec.ac.jp>
2024-11-19 11:56:36 +0900

    途中

--- PG3_00_01/main.cpp
+++ PG3_00_01/main.cpp
@@ -1,10 +1,23 @@
+//リストで駅名の配列を定義して表示するサンプル
+//このサンプルに2回駅を追加して2回for文で表示する処理を追加する
 #include <stdio.h>
-//#include <Windows.h> 
+#include <iostream>
+#include <list>
+
+using namespace std;
 
 int main() {
-	//SetConsoleOutputCP(65001);
-	char str[] = "こんにちは！";
-	printf("%s", str);
-	return 0;
 
+	list<const char*>rosen = { "田端","日暮里","西日暮里" };
+
+	for (/*イテレータの定義*/ itr = rosen.begin();
+		itr != rosen.end();
+		itr++)
+	{
+		cout << *itr << endl;
+	}
+
+	return 0;
 }
+
+
