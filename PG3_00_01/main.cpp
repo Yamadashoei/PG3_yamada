e5ac6f7351f4f39339481da24714c6712d5c84f8
Yamadashoei <k023g0053@g.neec.ac.jp>
2024-11-19 11:57:37 +0900

    途中

--- PG3_00_01/main.cpp
+++ PG3_00_01/main.cpp
@@ -1,10 +1,36 @@
 #include <stdio.h>
-//#include <Windows.h> 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 int main() {
-	//SetConsoleOutputCP(65001);
-	char str[] = "こんにちは！";
-	printf("%s", str);
-	return 0;
 
+	vector<string>meibo = {/*添付の学生番号配列*/ };
+
+	cout << "全要素を表示します(ソート前)" << endl;
+
+	// 全要素を表示
+	for (int i = 0; i < meibo.size(); i++)
+	{
+		cout << meibo[i] << endl;
+	}
+
+	// 昇順に並べ替え
+	cout << endl << "全要素をソートします" << endl << endl;
+
+	sort(/*meiboの初めから最後までをソート*/);
+
+	cout << "全要素を表示します(ソート後)" << endl;
+
+	// 全要素を表示
+	for (int i = 0; i < meibo.size(); i++)
+	{
+		cout << meibo[i] << endl;
+	}
+
+	return 0;
 }
+
+
