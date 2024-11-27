c303ffada247772d0699d34e8c1377c5c42c0a9c
Yamadashoei <k023g0053@g.neec.ac.jp>
2024-11-27 22:15:30 +0900

    02_04_途中

--- PG3_00_01/main.cpp
+++ PG3_00_01/main.cpp
@@ -1,58 +1,31 @@
 #include <stdio.h>
-
-class /*親クラス*/
-{
-public:
-	virtual void /*関数名*/() = 0;
-	virtual void /*関数名*/() = 0;
-
-protected:
-	float size;
-	float radius;
-};
-
-class /*円クラス*/ :public /*親クラス*/
-{
-public:
-	void Size();
-	void Draw() { printf("面積 %f\n", size); };
-};
-
-class /*四角クラス*/ :public /*親クラス*/
-{
-public:
-	void Size() override;
-	void Draw() { printf("面積 %f\n", size); };
-};
-
-void /*親クラス*/::Size() {}
-
-void /*円クラス*/::Size()
-{
-	radius = 5.0f;
-	printf("円の半径 %f\n", radius);
-	size = radius * radius * 3.14f;
-}
-
-void /*四角クラス*/::Size()
-{
-	radius = 5.0f;
-	printf("矩形の半径 %f\n", radius);
-	size = radius * 2.0f * radius * 2.0f;
-}
+#include "IShape.h"
+#include "Circle.h"
+#include "Rectangle.h"
 
 int main(void) {
 
-	/*親クラス*/ *ishape[2] = { new /*円クラス*/ ,new /*四角クラス*/ };
+	IShape* shape[2];
+	for (int i = 0; i < 2; i++) {
+		if (i == 0) {
+			shape[i] = new Circle;
+		}
+		else {
+			shape[i] = new Rectangle;
+		}
+	}
 
-	ishape[0]->Size();
-	ishape[1]->Size();
+	printf("円の半径:5\n短形の底辺:2\n短形の高さ:4\n");
 
-	ishape[0]->Draw();
-	ishape[1]->Draw();
+	for (int i = 0; i < 2; i++) {
+		shape[i]->Size();
+	}
 
-	delete ishape[0];
-	delete ishape[1];
+	printf("\n");
+
+	for (int i = 0; i < 2; i++) {
+		shape[i]->Draw();
+	}
 
 	return 0;
 }
